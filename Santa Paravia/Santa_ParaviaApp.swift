f047f25c41742a0dab23e412e5b50ac1bbcd5558
bmoxey <bmoxey@gmail.com>
2023-08-18 16:45:12 +1000

    Initial Commit

---
+++ Santa Paravia/Santa_ParaviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Santa_ParaviaApp.swift
+//  Santa Paravia
+//
+//  Created by Brett Moxey on 18/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct Santa_ParaviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
